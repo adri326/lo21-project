3eacf1a703f1b1a706058aed099569173a496545
Adrien Burgun <adrien.burgun@orange.fr>
2020-12-16 13:44:27 +0100

    :sparkles: Upgrade list and add initial AST

--- ext/src/parse.c
+++ ext/src/parse.c
@@ -1,2 +1,6 @@
 #include <string.h>
 #include "parse.h"
+
+DECL_BT_SOURCES(subexpr_ast);
+DECL_LL_SOURCES(ccl_symbol);
+DECL_LL_SOURCES(expr_ast);
--- ext/src/parse.h
+++ ext/src/parse.h
@@ -1,6 +1,12 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+#include <stdbool.h>
+#include <rule.h>
+#include <btree.h>
+#include <linkedlist.h>
+#include <inttypes.h>
+
 #define BEGIN_PAREN '('
 #define END_PAREN ')'
 
@@ -18,5 +24,36 @@
 #define THUS "=>"
 #define NOT "!"
 #define ERR "error"
+
+enum EXPR_KIND {
+    KIND_OR,
+    KIND_NOT,
+    KIND_AND,
+    KIND_SYMBOL,
+    KIND_ERROR
+};
+
+struct subexpr_ast {
+    enum EXPR_KIND kind;
+    char symbol[SYMBOL_LEN];
+};
+typedef struct subexpr_ast subexpr_ast;
+
+struct ccl_symbol {
+    bool negate;
+    char symbol[SYMBOL_LEN];
+};
+typedef struct ccl_symbol ccl_symbol;
+
+DECL_BT(subexpr_ast);
+DECL_LL(ccl_symbol);
+
+struct expr_ast {
+    BT(subexpr_ast)* condition;
+    LL(ccl_symbol)* conclusion;
+};
+typedef struct expr_ast expr_ast;
+
+DECL_LL(expr_ast);
 
 #endif // PARSE_H
