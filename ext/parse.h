83b4c90dfa5fa436908aa3007f55b584f7758df7
Adrien Burgun <adrien.burgun@orange.fr>
2020-12-16 11:25:44 +0100

    :sparkles: Ext grammar and #defines in ext/parse.h

---
+++ ext/parse.h
@@ -0,0 +1,22 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+#define BEGIN_PAREN '('
+#define END_PAREN ')'
+
+#define BEGIN_STRING '"'
+#define END_STRING '"'
+
+#define BEGIN_COMMENT '{'
+#define END_COMMENT '}'
+
+#define TERMINAL ';'
+#define UNDERSCORE '_'
+
+#define AND "&&"
+#define OR "||"
+#define THUS "=>"
+#define NOT "!"
+#define ERR "error"
+
+#endif // PARSE_H
